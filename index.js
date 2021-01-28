e0c39546a9e74f52004f1b2f9e828afb224a8daf
mzteepowell <mzteepowell@gmail.com>
2021-01-27 21:28:03 -0500

    Completed Task 1 - 3

--- index.js
+++ index.js
@@ -39,16 +39,27 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
-  }
- 
- 
+ function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
 
-  
-  
-  
-  
+Person.prototype.eat = function(food) {
+  if (this.stomach.length < 10) {
+  this.stomach.push(food);
+  }
+  return this.stomach;
+}
+
+Person.prototype.poop = function() {
+  return this.stomach = [];
+}
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+}
+
   /*
     TASK 2
       - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -63,10 +74,25 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+
+Car.prototype.fill = function(gallons) {
+ return this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function(distance) {
+  if (this.tank <= 0) {
+    return `I ran out of fuel at ${this.odometer} mile!`;
   }
-  
+  else {
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank - (this.milesPerGallon / distance);
+  }
+}
   
   /*
     TASK 3
@@ -75,15 +101,21 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+   this.name = name; 
+   this.age = age; 
+   this.favoriteToy = favoriteToy;
   }
- 
+ Baby.prototype = Object.create(Person.prototype);
+
+ Baby.prototype.play  = function() {
+   return `Playing with ${this.favoriteToy}`;
+ }
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
+    1. The "this" keyword
     2. 
     3. 
     4. 
